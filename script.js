de215895646e94473dc09d1b9ccba6c1db27cc8c
Virtualez <90089636+Virtualez@users.noreply.github.com>
2022-03-29 14:29:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const text = document.querySelector('.text');
+
+// $& - спецсимвол, вставляет все найденное совпадение.
+
+text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
+
+text.addEventListener('mouseover', function(event) {
+    if (event.target.tagName != 'SPAN') return;
+    event.target.classList.add('active');
+
+});
